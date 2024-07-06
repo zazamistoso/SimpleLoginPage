a00f5d550692d6a98f9eaa2b71fefc0fbeffb8cb
Amistoso <mzalamistoso@tip.edu.ph>
2024-07-07 03:01:03 +0800

    You can input username and password. A pop up will also show if forget password is cliked.

--- app/src/main/java/com/example/loginpage/MainActivity.java
+++ app/src/main/java/com/example/loginpage/MainActivity.java
@@ -1,24 +1,69 @@
 package com.example.loginpage;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
+    private EditText usernameText;
+    private EditText passwordText;
+    private Button loginButton;
+    private Button forgotPasswordButton;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.Main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        try {
+            usernameText = findViewById(R.id.username);
+            passwordText = findViewById(R.id.password);
+            loginButton = findViewById(R.id.loginButton);
+            forgotPasswordButton = findViewById(R.id.forgotPass);
+
+        } catch (NullPointerException e){
+            Log.e("MainActivity", "Error finding view", e);
+        }
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = usernameText.getText().toString();
+                String password = passwordText.getText().toString();
+                if ("TIP".equals(username) && "12345".equals(password)) {
+                    Toast.makeText(MainActivity.this, "Login Successful!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
+                }
+            }
         });
+
+        forgotPasswordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "The password is 1234567", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    private void showDialog(String message) {
+        AlertDialog.Builder builder = new
+                AlertDialog.Builder(MainActivity.this);
+        builder.setMessage(message)
+                .setCancelable(false)
+                .setPositiveButton("OK", new
+                        DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        });
+        AlertDialog alert = builder.create();
+        alert.show();
     }
 }
